bf2e71fc8c0f27fa63559f25f5b4ef732670345c
yuhaohan <yuhaohan@btte.net>
2020-06-10 21:45:03 +0800

    完成员工添加功能

--- src/main/java/com/yhh/springbootwebdemo/controller/EmployeeController.java
+++ src/main/java/com/yhh/springbootwebdemo/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.yhh.springbootwebdemo.controller;
 
+import com.yhh.springbootwebdemo.dao.DepartmentDao;
 import com.yhh.springbootwebdemo.dao.EmployeeDao;
+import com.yhh.springbootwebdemo.entities.Department;
 import com.yhh.springbootwebdemo.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -19,11 +22,27 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeDao employeeDao;
 
+	@Autowired
+	private DepartmentDao departmentDao;
+
 	@GetMapping(value = "/emps")
-	public String list(Model model){
+	public String list(Model model) {
 		Collection<Employee> all = employeeDao.getAll();
 		model.addAttribute("emps", all);
 		return "emp/list";
 	}
 
+	@GetMapping(value = "/emp")
+	public String toAddPage(Model model) {
+		Collection<Department> departments = departmentDao.getDepartments();
+		model.addAttribute("depts", departments);
+		return "emp/add";
+	}
+
+	@PostMapping(value = "/emp")
+	public String addEmp(Employee employee) {
+		System.out.println(employee);
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
 }
