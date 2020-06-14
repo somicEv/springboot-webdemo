0fc1b9a5f51b05569a1a50e7ea41ae482cd9753a
yuhaohan <yuhaohan@btte.net>
2020-06-14 10:40:39 +0800

    完成员工增删改查操作

--- src/main/java/com/yhh/springbootwebdemo/controller/EmployeeController.java
+++ src/main/java/com/yhh/springbootwebdemo/controller/EmployeeController.java
@@ -7,8 +7,7 @@ import com.yhh.springbootwebdemo.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -16,6 +15,7 @@ import java.util.Collection;
  * @author: yuhaohan
  * @date: 星期二
  */
+
 @Controller
 public class EmployeeController {
 
@@ -43,6 +43,29 @@ public class EmployeeController {
 	public String addEmp(Employee employee) {
 		System.out.println(employee);
 		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	@GetMapping(value = "/emp/{id}")
+	public String toEditPage(@PathVariable("id") Integer id, Model model) {
+		Employee employee = employeeDao.get(id);
+		model.addAttribute("emp", employee);
+		Collection<Department> departments = departmentDao.getDepartments();
+		model.addAttribute("depts", departments);
+		// 重用ADD页面
+		return "emp/add";
+	}
+
+	@PutMapping(value = "/emp")
+	public String updateEmployee(Employee employee) {
+		System.out.println("要修改的员工信息：" + employee);
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	@PostMapping(value = "/emp/{id}")
+	public String deleteEmployee(@PathVariable("id") Integer id) {
+		employeeDao.delete(id);
 		return "redirect:/emps";
 	}
 }
